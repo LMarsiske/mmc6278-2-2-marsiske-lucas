767b2984d89b629e6a0c4f62c57cd43a1cbd8e2c
Lucas Marsiske <47642996+LMarsiske@users.noreply.github.com>
2022-05-18 15:15:09 -0400

    Wrote greet() function

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function greet() {
+  let name = prompt("What is your name?");
+  alert(`Hello, ${name}`);
+  let age = parseInt(prompt("How old are you?"));
+  let birthYear = null;
+  let currentYear = new Date().getFullYear();
+  if (window.confirm("Have you had a birthday yet this year?")) {
+    birthYear = currentYear - age;
+  } else {
+    birthYear = currentYear - (age + 1);
+  }
+  alert(`You were born in ${birthYear}!`);
+}
